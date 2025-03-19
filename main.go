c76283e21da2f360745933d30e6b6483eba90517
Maath-Lima <math3226@gmail.com>
2025-03-18 21:38:54 -0300

    Pointers and Offset get operations

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
+import "encoding/binary"
+
+// | type | nkeys | pointers | offsets | key-values | unused |
+// | 2B | 2B | nkeys * 8B | nkeys * 2B | ... | |
+//
+// | klen | vlen | key | val |
+// | 2B | 2B | ... | ... |
+
 const HEADER = 4
 
 const BTREE_PAGE_SIZE = 4096
 const BTREE_MAX_KEY_SIZE = 1000
 const BTREE_MAX_VAL_SIZE = 3000
+
+const (
+	BNODE_NODE = 1
+	BNODE_LEAF = 2
+)
 
 type BNode []byte
 type BTree struct {
@@ -14,6 +27,55 @@ type BTree struct {
 	del  func(uint64)
 }
 
+func (node BNode) btype() uint16 {
+	return binary.LittleEndian.Uint16(node[0:2])
+}
+
+func (node BNode) nkeys() uint16 {
+	return binary.LittleEndian.Uint16(node[2:4])
+}
+
+func (node BNode) setHeader(btype uint16, nkeys uint16) {
+	binary.LittleEndian.PutUint16(node[0:2], btype)
+	binary.LittleEndian.PutUint16(node[2:4], nkeys)
+}
+
+// Atention point
+// pointers
+func (node BNode) getPtr(idx uint16) uint64 {
+	assert(idx < node.nkeys())
+
+	pos := HEADER + 8*idx
+	return binary.LittleEndian.Uint64(node[pos:])
+}
+
+func (node BNode) setPtr(idx uint16, val uint64)
+
+// offset list
+func offsetPos(node BNode, idx uint16) uint16 {
+	assert(1 <= idx && idx <= node.nkeys())
+
+	return HEADER + 8*node.nkeys() + 2*(idx-1)
+}
+
+func (node BNode) getOffset(idx uint16) uint16 {
+	if idx == 0 {
+		return 0
+	}
+
+	return binary.LittleEndian.Uint16(node[offsetPos(node, idx):])
+}
+
+func (node BNode) setOffset(idx uint16, offset uint16)
+
+// key-values
+func (node BNode) kvPos(idx uint16) uint16 {
+	assert(idx <= node.nkeys())
+
+	return HEADER + 8*node.nkeys() + 2*node.nkeys() + node.getOffset(idx)
+}
+
+// main
 func init() {
 	node1max := HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE
 
