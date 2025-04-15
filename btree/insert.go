41d5b49fd21c804accfad4281b4cc7b6b1c77c9e
Maath-Lima <math3226@gmail.com>
2025-04-14 21:09:44 -0300

    Splitting nodes

--- btree/insert.go
+++ btree/insert.go
@@ -1,6 +1,9 @@
 package btree
 
-import "encoding/binary"
+import (
+	"database/utils"
+	"encoding/binary"
+)
 
 // add a new node to a leaf node
 // copy-on-write
@@ -53,4 +56,62 @@ func nodeAppendKV(new BNode, idx uint16, ptr uint64, key []byte, val []byte) {
 
 	// update the offset value for the next key
 	new.setOffset(idx+1, new.getOffset(idx)+4+uint16(len(key)+len(val)))
+}
+
+// Split an oversized node into 2 nodes. The 2nd node always fits.
+// ...we can use the half position as an initial guess, then move it left or right if the half is too large
+func nodeSplit2(left BNode, right BNode, old BNode) {
+	utils.Assert(old.nkeys() >= 2)
+
+	// the initial guess
+	nleft := old.nkeys() / 2
+	// try to fit the left half
+	left_bytes := func() uint16 {
+		return 4 + 8*nleft + 2*nleft + old.getOffset(nleft)
+	}
+	for left_bytes() > BTREE_PAGE_SIZE {
+		nleft--
+	}
+	utils.Assert(nleft >= 1)
+
+	// try to fit the right half
+	right_bytes := func() uint16 {
+		return old.nbytes() - left_bytes() + 4
+	}
+	for right_bytes() > BTREE_PAGE_SIZE {
+		nleft++
+	}
+	utils.Assert(nleft < old.nkeys())
+
+	nright := old.nkeys() - nleft
+
+	// new nodes
+	left.setHeader(old.btype(), nleft)
+	right.setHeader(old.btype(), nright)
+	nodeAppendRange(left, old, 0, 0, nleft)
+	nodeAppendRange(right, old, 0, nleft, nright)
+	// NOTE: the left half may be still too big
+	utils.Assert(right.nbytes() <= BTREE_PAGE_SIZE)
+}
+
+// split a node if it's too big. the results are 1~3 nodes.
+func nodeSplit3(old BNode) (uint16, [3]BNode) {
+	if old.nbytes() < BTREE_PAGE_SIZE {
+		old = old[:BTREE_PAGE_SIZE]
+		return 1, [3]BNode{old} // not split
+	}
+
+	left := BNode(make([]byte, 2*BTREE_PAGE_SIZE))
+	right := BNode(make([]byte, BTREE_PAGE_SIZE))
+	nodeSplit2(left, right, old)
+	if left.nbytes() <= BTREE_PAGE_SIZE {
+		left = left[:BTREE_PAGE_SIZE]
+		return 2, [3]BNode{left, right}
+	}
+
+	leftleft := BNode(make([]byte, BTREE_PAGE_SIZE))
+	middle := BNode(make([]byte, BTREE_PAGE_SIZE))
+	nodeSplit2(leftleft, middle, left)
+	utils.Assert(leftleft.nbytes() <= BTREE_PAGE_SIZE)
+	return 3, [3]BNode{leftleft, left, right}
 }
