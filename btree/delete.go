f1fc26f8e8462341e06c63f77a569ca3d10c3f68
Maath-Lima <math3226@gmail.com>
2025-04-24 21:40:27 -0300

    insert and delete base funcs

---
+++ btree/delete.go
@@ -0,0 +1,5 @@
+package btree
+
+func (tree *BTree) Delete(key []byte) (bool, error) {
+
+}
--- btree/insert.go
+++ btree/insert.go
@@ -153,3 +153,37 @@ func treeInsert(tree *BTree, node BNode, key []byte, val []byte) BNode {
 
 	return new
 }
+
+// insert a new key or update an existing key
+func (tree *BTree) Insert(key []byte, val []byte) {
+	// 1. check the length limit imposed by the node format
+	utils.Assert(len(key) <= BTREE_MAX_KEY_SIZE)
+	utils.Assert(len(val) <= BTREE_MAX_VAL_SIZE)
+
+	if tree.root == 0 {
+		root := BNode(make([]byte, BTREE_PAGE_SIZE))
+		root.setHeader(BNODE_LEAF, 2)
+		// dummy key for tree covering the whole key space
+		nodeAppendKV(root, 0, 0, nil, nil) // sentinel value, to nodeLookupLE always find a position
+		nodeAppendKV(root, 1, 0, key, val)
+		tree.root = tree.new(root)
+		return
+	}
+
+	node := treeInsert(tree, tree.get(tree.root), key, val)
+	nsplit, split := nodeSplit3(node)
+	tree.del(tree.root)
+
+	if nsplit > 1 {
+		// add a new level
+		root := BNode(make([]byte, BTREE_PAGE_SIZE))
+		root.setHeader(BNODE_NODE, nsplit)
+		for i, knode := range split[:nsplit] {
+			ptr, key := tree.new(knode), knode.getKey(0)
+			nodeAppendKV(root, uint16(i), ptr, key, nil)
+		}
+		tree.root = tree.new(root)
+	} else {
+		tree.root = tree.new(split[0])
+	}
+}
